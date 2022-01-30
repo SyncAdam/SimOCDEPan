ef6f892cd0c218136ed6ab53c3ee25150c53042c
SyncAdam <61192656+SyncAdam@users.noreply.github.com>
2022-01-30 10:04:35 +0100

    Add files via upload

--- infospratiques.php
+++ infospratiques.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="style.css">
     </head>
 
-    <body style="background: url('mainbg.jpg') no-repeat; background-color: black; background-size: 2220px 1380px; background-position-x: center; background-attachment: fixed;">
+    <body style="background: url('infoskep.png') no-repeat; background-size: cover; background-position-x: center; background-attachment: fixed;  ">
         <header>
         <div class="container">
             <div id="branding">
@@ -20,16 +20,18 @@
                 <li><a href="programme.html"><p>Programme</p></a></li>
                 <li><a href="partenaires.html"><p>Partenaires</p></a></li>
                 <li class="current"><a href="infospratiques.php"><p style="white-space: nowrap;">Infos Pratiques</p></a></li>
+                <li><a href="documents.html"><p>Documents</p></a></li>
             </ul>
             </nav>
         </div>
         </header>
 
         <section id="infospratiquesshowcase">
-            <div class="showcasecontainer">
-            <h2>conférence OCDE au lycée <span style="color: #2685C9;">Kölcsey</span></h2>
+            <div class="showcasecontainer" style="padding-top: 80px">
+            <h2>conférence OCDE au lycée <span style="color: #8bd4ff;">Kölcsey</span></h2>
             <h1>Infos Pratiques</h1>
             </div>
+            <div style="padding: 50px 0 50px 0"></div>
         </section>
 
         <section id="infospratiquescontent">
@@ -50,12 +52,20 @@
                 </div>
             </div>
             <div class="infospratiquesbox">
+                <h2 style="color: red">Mesures sanitaires</h2>
+                <p style="color: red">Les mesures sanitaires en vigueur sont plainement appliquées dans notre établissement:</p>
+                <ul>
+                    <li style="color: red">Masque obligatoire</li>
+                    <li style="color: red">Vaccination complète pour les professeurs accompagnateurs</li>
+                </ul>
+            </div>
+            <div class="infospratiquesbox">
                 <h2>COMMENT SE RENDRE AU LYCÉE?</h2>
                 <p>Budapest VI. Kerületi Kölcsey Ferenc Gimnázium, Budapest<br>
                     1063 Budapest<br>
                     Munkácsy Mihály utca 26</p>
             </div>
-            <div class="infospratiquesbox">
+            <div class="infospratiquesbox"">
                 <h4>Accès en transports publics.</h4>
                 <p>Arrêt <span style="color: chartreuse;">Hősök tere</span> (bus 20E, 30 depuis Keleti pályaudvar, métro 1)<br>
                 Arrêt <span style="color: chartreuse;">Nyugati pályaudvar</span> + 10-15 minutes à pied (métro 3, tram 4/6)<br>
@@ -94,19 +104,16 @@
            var infwinoptcont =  "<div style=\"text-align:left;\"><p style=\"margin:0; color: black; font-size:12px;\">Lycée Kölcsey Ferenc, Budapest</p><a href=\"https://www.kolcsey-bp.hu/index.php/kapcsolat\" style=\"font-size: 11px\">Site Internet de l'établissement</a></div><div><a href=\"https://www.google.com/maps/dir/?api=1&destination=Budapest,%20Munk%C3%A1csy%20Mih%C3%A1ly%20u.%2026,%201063%20Hongrie\" style=\"font-size: 11px; margin-top:10px\">Direction</a></div>";
            
            //infowindow
-           //var infwinoptcont = "<div><p>This is a Window mfs</p></div>";
            var infwinopt = {
                 content: infwinoptcont,
                 maxWidth: 100,
             };
             var infowindow = new google.maps.InfoWindow(infwinopt);
             infowindow.open(map, markerk);
-            /*
             //listener
             markerk.addListener("click", function(){
-                ;
+                infowindow.open(map, markerk);
             });
-            */
         </script>
     </body>
 </html>
